e9a730f98288ad2c6b3611dba3c393dfc985da22
Valentin <43175508+V2i@users.noreply.github.com>
2019-05-14 22:55:15 +0200

    creation route

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('comment','CommentairesController');
+Route::resource('house','MaisonsController');
+Route::resource('reservation','ReservationsController');
+Route::resource('picture','PhotosController');
