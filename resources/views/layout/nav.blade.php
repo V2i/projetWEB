197167b0ae6537c5a2d902b90a39439b46eda8cf
Valentin <43175508+V2i@users.noreply.github.com>
2019-05-14 14:36:29 +0200

    custom css update and fix

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -33,8 +33,8 @@
         <!-- Fonts & style-->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-        <link rel="stylesheet" href="{{secure_asset('css/custom.css')}}">
-        <link rel="stylesheet" href="{{URL::asset('css/custom.css')}}">
+        <link rel="stylesheet" href="{{secure_asset('css/custom.css')}}"> <!--heroku-->
+        <link rel="stylesheet" href="{{URL::asset('css/custom.css')}}"> <!--local-->
         <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
 
     </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-
+<div class="container main-container">
 Vita est illis semper in fuga uxoresque mercenariae conductae ad tempus ex pacto atque, ut sit species matrimonii, dotis nomine futura coniunx hastam et tabernaculum offert marito, post statum diem si id elegerit discessura, et incredibile est quo ardore apud eos in venerem uterque solvitur sexus.
 
 Haec subinde Constantius audiens et quaedam referente Thalassio doctus, quem eum odisse iam conpererat lege communi, scribens ad Caesarem blandius adiumenta paulatim illi subtraxit, sollicitari se simulans ne, uti est militare otium fere tumultuosum, in eius perniciem conspiraret, solisque scholis iussit esse contentum palatinis et protectorum cum Scutariis et Gentilibus, et mandabat Domitiano, ex comite largitionum, praefecto ut cum in Syriam venerit, Gallum, quem crebro acciverat, ad Italiam properare blande hortaretur et verecunde.
@@ -29,4 +29,5 @@ Siquis enim militarium vel honoratorum aut nobilis inter suos rumore tenus esset
 Ut enim benefici liberalesque sumus, non ut exigamus gratiam (neque enim beneficium faeneramur sed natura propensi ad liberalitatem sumus), sic amicitiam non spe mercedis adducti sed quod omnis eius fructus in ipso amore inest, expetendam putamus.
 
 Metuentes igitur idem latrones Lycaoniam magna parte campestrem cum se inpares nostris fore congressione stataria documentis frequentibus scirent, tramitibus deviis petivere Pamphyliam diu quidem intactam sed timore populationum et caedium, milite per omnia diffuso propinqua, magnis undique praesidiis conmunitam.
+</div>
 @endsection
