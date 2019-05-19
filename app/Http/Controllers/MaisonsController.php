cc1ed12e5749ddfe1947e3edd4e46c47ac40d5c3
Valentin <43175508+V2i@users.noreply.github.com>
2019-05-19 19:41:36 +0200

    caroussel fonctionel

--- app/Http/Controllers/MaisonsController.php
+++ app/Http/Controllers/MaisonsController.php
@@ -71,10 +71,9 @@ class MaisonsController extends Controller
     {
         //$maison=DB::table('maisons')->where('id',$id)->join('users','maisons.user_id', '=', 'users.id')->first();
         //$maison=DB::table('users')->join('maisons','users.id', '=', 'maisons.user_id')->first();
-        $user_id=DB::table('maisons')->where('id',$id)->pluck('user_id');
-        $maison=Maison::find($id);
+        $user_id=DB::table('maisons')->where('id',$id)->pluck('user_id');        $maison=Maison::find($id);
         $user=User::where('id',$user_id)->first();
-        $photos=Photo::where('id', $maison -> maison_id)->get();
+        $photos=Photo::where('maison_id', $id)->get();
 
         return  view('house',[
             'maison' => $maison,
--- resources/views/house.blade.php
+++ resources/views/house.blade.php
@@ -28,13 +28,13 @@
             <div class="col-md-6">
                 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
                     <ol class="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                    </ol>
-                    <div class="carousel-inner">
                         @foreach ($photos as $photo)
-                            <div class="carousel-item active">
+                            <li data-target="#carouselExampleIndicators" data-slide-to="{{ $loop -> index}}" class="{{ $loop->first ? 'active' : '' }}"></li>
+                        @endforeach
+                    </ol>
+                    <div class="carousel-inner" role="listbox">
+                        @foreach ($photos as $photo)
+                            <div class="carousel-item {{ $loop->first ? ' active' : '' }}">
                                 <img class="d-block w-100" src="{{ $photo -> url }}" alt="{{ $photo -> type_photo }}">
                             </div>
                         @endforeach
